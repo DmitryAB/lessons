bd2f9a81af5cb20bcea0123416c9aaa5c448d771
DmitryAB <dmitriy.generalov@inbox.ru>
2012-06-10 15:06:06 +0400

    binomial.py

--- binomial.py
+++ binomial.py
@@ -6,15 +6,12 @@ class Binomial:
   self.b=float(b)
   self.c=float(c)
  def __str__(self):
-  binom = str(self.a) + "*x*x" 
-  if self.b>0.0:
-   binom += "+" + str(self.b)+"*x"
-  if self.b<0.0:
-   binom += str(self.b)+"*x"
-  if self.c>0.0 :
-   binom += "+" + str(self.c)
-  if self.c<0.0:
-   binom += str(self.c)
+  binom = str(self.a) + "*x*x"
+  signed_number = lambda x: str(x) if x<0 else "+" + str(x) 
+  if self.b!=0.0:
+   binom += signed_number(self.b) +"*x"
+  if self.c!=0.0:
+    binom += signed_number(self.c)
   return binom
  def real_roots(self):
   self.d=self.b*self.b-4*self.a*self.c
@@ -24,13 +21,6 @@ class Binomial:
   if self.d==0:
    return [(-self.b+math.sqrt(self.d))/(2*self.a)]
   return []
-"""a=input("Input a:")
-b=input("Input b:")
-c=input("Input c:")
-binom = Binomial(a,b,c)
-print(binom)
-print(binom.real_roots())
-"""
 
 
 
