5e0c1595931d963637237d7e28bb4bdc5bf30cfc
DmitryAB <dmitriy.generalov@inbox.ru>
2012-06-10 10:47:12 +0400

    test_binomial.py

--- test_binomial.py
+++ test_binomial.py
@@ -1,14 +1,46 @@
 from binomial import Binomial
 import unittest
 
-class TestBenomial(unittest.TestCase):
+class TestBinomialSingle(unittest.TestCase):
    def setUp(self):
-       self.a=1
-       self.b=-2
-       self.c=1
+       self.binomial = Binomial(1,-2,1)
    def test_real_roots(self):
-       self.assertEqual(Binomial(self.a,self.b,self.c).real_roots(), [1.0])
+       self.assertEqual(self.binomial.real_roots(), [1.0])
    def test_str(self):
-       self.assertEqual(str(Binomial(self.a,self.b,self.c)),'1.0*x*x-2.0*x+1.0')
+       self.assertEqual(str(self.binomial),'1.0*x*x-2.0*x+1.0')
+
+class  TestBinomialWithTwoRoots(unittest.TestCase):
+   def setUp(self):
+       self.binomial = Binomial(1,2,-3)
+   def test_real_roots(self):
+       self.assertEqual(self.binomial.real_roots(), [1.0,-3.0])
+   def test_str(self):
+       self.assertEqual(str(self.binomial),'1.0*x*x+2.0*x-3.0')
+
+class TestWinomialWithoutRoots(unittest.TestCase):
+   def setUp(self):
+       self.binomial = Binomial(1,2,3)
+   def test_real_roots(self):
+       self.assertEqual(self.binomial.real_roots(), [])
+   def test_str(self):
+       self.assertEqual(str(self.binomial),'1.0*x*x+2.0*x+3.0')
+
+class TestBinomialWithoutSecondCoefficient(unittest.TestCase):
+   def setUp(self):
+       self.binomial = Binomial(1,0,-4)
+   def test_real_roots(self):
+       self.assertEqual(self.binomial.real_roots(), [2.0,-2.0])
+   def test_str(self):
+       self.assertEqual(str(self.binomial),'1.0*x*x-4.0')
+
+class TestBinomialWithoutThirdCoefficient(unittest.TestCase):
+   def setUp(self):
+       self.binomial = Binomial(1,2,0)
+   def test_real_roots(self):
+       self.assertEqual(self.binomial.real_roots(), [0.0,-2.0])
+   def test_str(self):
+       self.assertEqual(str(self.binomial),'1.0*x*x+2.0*x')
+
 if __name__ == '__main__':
     unittest.main()
+
