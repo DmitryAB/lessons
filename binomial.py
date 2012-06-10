b95629c1985aca06fd1e9485459bdeeb8f5cba74
DmitryAB <dmitriy.generalov@inbox.ru>
2012-06-10 10:45:56 +0400

    binomial.py

--- binomial.py
+++ binomial.py
@@ -5,18 +5,22 @@ class Binomial:
   self.a=float(a)
   self.b=float(b)
   self.c=float(c)
- def __str__(self):
+ def __str__(self): 
   binom = str(self.a) + "*x*x"
-  if self.b>0 :
+  if self.b>0.0:
    binom +="+" + str(self.b)+"*x"
   else:
-   binom += str(self.b) + "*x"
-  if self.c>0 :
+   if self.b<0.0:
+    binom += str(self.b)+"*x"
+  if self.c>0.0 :
    binom += "+" + str(self.c)
   else: 
-   binom += str(self.c)
+   if self.c<0.0:
+    binom += str(self.c)
   return binom
  def real_roots(self):
+  if self.a==0:
+   return [-self.c/self.b]
   self.d=self.b*self.b-4*self.a*self.c
  # print(self.d)
   if self.d>0:
@@ -26,14 +30,13 @@ class Binomial:
    return [(-self.b+math.sqrt(self.d))/(2*self.a)]
   else:
    return []
-"""a=str(random.uniform(-100,100)) #input("Input a:")
-b=str(random.uniform(-100,100)) #input("Input b:")
-c=str(random.uniform(-100,100)) #input("Input c:")
+"""a=input("Input a:")
+b=input("Input b:")
+c=input("Input c:")
 binom = Binomial(a,b,c)
 print(binom)
 print(binom.real_roots())
 """
-
 
 
 
